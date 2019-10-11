8cf5d641b7735072ad85afd372487191849d4ad5
jerryzj <jerry0715no14@gmail.com>
2019-10-11 14:26:57 +0800

    WIP, oe_sort parallel algo

--- hw1/oe.cc
+++ hw1/oe.cc
@@ -7,10 +7,13 @@ OE_sort::OE_sort(int rank, int task_num, int file_size, const char *input_file,
     : rank(rank), task_num(task_num), file_size(file_size),
       input_file(input_file),
       output_file(output_file), global_sorted(false), local_sorted(false) {
+    
+    // Data partition
     num_per_task = file_size / task_num;
-    res = file_size % task_num; // remaining parts
+    res = file_size % task_num; 
     offset = num_per_task * rank + std::min(rank, res);
     size = num_per_task + (rank < res); // remaining parts will divided by all
+    
     // Calculate left/right buffer size
     if(rank == 0){ left_size = 0;}
     else{
@@ -20,18 +23,31 @@ OE_sort::OE_sort(int rank, int task_num, int file_size, const char *input_file,
     else {
         right_size = num_per_task + ((rank + 1) < res);
     }
-    buffer = (new float[size]);
+
+    neighbor_buffer = new float[std::max(left_size, right_size)];
+    buffer0 = new float[size];
+    buffer1 = new float[size];
+    if(rank % 2){
+        curr_buffer = buffer0;
+    }
+    else {
+        curr_buffer = buffer1;
+    }
     // Debug
     //cout << "Rank = "<<rank<< " Size = "<<size<<" lSize = " << left_size << " rSize = "<< right_size <<endl;
 }
 
-OE_sort::~OE_sort() { delete[](buffer); }
+OE_sort::~OE_sort() { 
+    delete[](buffer0);
+    delete[](buffer1);
+    delete[](neighbor_buffer);
+}
 
 void OE_sort::read_file() {
     MPI_File fh;
     MPI_File_open(MPI_COMM_WORLD, input_file, MPI_MODE_RDONLY, MPI_INFO_NULL,
                   &fh);
-    MPI_File_read_at_all(fh, offset * sizeof(float), buffer, size, MPI_FLOAT,
+    MPI_File_read_at_all(fh, offset * sizeof(float), curr_buffer, size, MPI_FLOAT,
                          MPI_STATUS_IGNORE);
     MPI_File_close(&fh);
     // debug
@@ -47,32 +63,39 @@ void OE_sort::write_file() {
     MPI_File fh;
     MPI_File_open(MPI_COMM_WORLD, output_file,
                   MPI_MODE_WRONLY | MPI_MODE_CREATE, MPI_INFO_NULL, &fh);
-    MPI_File_write_at_all(fh, offset * sizeof(float), buffer, size, MPI_FLOAT,
+    MPI_File_write_at_all(fh, offset * sizeof(float), curr_buffer, size, MPI_FLOAT,
                           MPI_STATUS_IGNORE);
     MPI_File_close(&fh);
 }
 
-bool OE_sort::odd_even(int state) {
-    bool sorted(true);
-    MPI_Request l_req, r_req;
-
-    int i(0), end(0);
-    for(; i < end; i+=2){
-        if(buffer[i] > buffer[i+1]){
-            sorted = false;
-            std::swap(buffer[i], buffer[i+1]);
+void OE_sort::sort() {
+    // use STL to sort local content
+    std::sort(curr_buffer, curr_buffer + size);
+    local_sorted = true;
+    // Split odd rank & even rank
+    if (rank % 2) {
+        while(!global_sorted){
+            local_sorted = local_sorted & do_left();
+            local_sorted = local_sorted & do_right();
+            // Sync sorting status
+            MPI_Allreduce(&local_sorted, &global_sorted, 1, MPI::BOOL, MPI_LAND,
+                          MPI_COMM_WORLD);
         }
     }
-    return sorted;
+    else {
+        while(!global_sorted){
+            local_sorted = local_sorted & do_right();
+            local_sorted = local_sorted & do_left();
+            MPI_Allreduce(&local_sorted, &global_sorted, 1, MPI::BOOL, MPI_LAND,
+                          MPI_COMM_WORLD);
+        }
+    }
 }
 
-void OE_sort::sort() {
-    while (!global_sorted) {
-        global_sorted = true;
-        local_sorted = local_sorted & odd_even(0);
-        local_sorted = local_sorted & odd_even(1);
-        // Question: MPI datatype for bool???
-        MPI_Allreduce(&local_sorted, &global_sorted, 1, MPI::BOOL, MPI_LAND,
-                      MPI_COMM_WORLD);
-    }
+bool OE_sort::do_left() {
+
+}
+
+bool OE_sort::do_right() {
+    
 }
--- hw1/oe.h
+++ hw1/oe.h
@@ -17,21 +17,26 @@ public:
     void sort();
 
 private:
-    bool odd_even(int state);
     int rank;         // My rank ID
     int task_num;     // Total ranks
     int file_size;    // File size, should be N * sizeof(float)
     int num_per_task; // Number of floats handled by a rank
     int res;          // Remaining part
     int size;         // Buffer size, num of float
-    int left_size;
-    int right_size;
-    int offset;       // Read start point
-    float *buffer;
+    int left_size;    // Data number of left rank
+    int right_size;   // Data number of right rank
+    int offset;       // Read starting point
+    float *&curr_buffer;
+    float *buffer0;
+    float *buffer1;
+    float *neighbor_buffer;
     bool global_sorted;
     bool local_sorted;
     const char *input_file;
     const char *output_file;
+
+    bool do_left();
+    bool do_right();
 };
 
 #endif
