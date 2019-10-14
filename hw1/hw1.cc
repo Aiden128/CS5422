e4f4b443df0a4fa3d5ed93023dfebe3a6734ffc6
jerryzj <jerry0715no14@gmail.com>
2019-10-14 21:48:18 +0800

    enable sort

--- hw1/hw1.cc
+++ hw1/hw1.cc
@@ -11,14 +11,14 @@ int main(int argc, char **argv) {
 
     int rank(0), task_num(0);
     // Start processing
-    MPI_Init(&argc,&argv);
+    MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &task_num);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
     OE_sort oe(rank, task_num, stoi(argv[1]), argv[2], argv[3]);
-    
+
     oe.read_file();
-    //oe.sort();
+    oe.sort();
     oe.write_file();
 
     MPI_Finalize();
