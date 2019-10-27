aedc688cb941b768f4ee36b548cc96b06bad0563
jerryzj <jerry0715no14@gmail.com>
2019-10-27 16:05:36 +0800

    Add hw2

---
+++ hw2/pthread.cc
@@ -0,0 +1,19 @@
+#define DEBUG
+#ifdef DEBUG
+#include <cassert>
+#endif
+#include <iostream>
+#include <pthread.h>
+#include <png.h>
+
+
+using namespace std;
+
+int main(int argc, char **argv) {
+
+    assert(argc == 9);
+
+    
+
+    return 0;
+}
