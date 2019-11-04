6381e5dfd6a5b5ff426e5d004626d696ddb38e95
jerryzj <jerry0715no14@gmail.com>
2019-11-04 20:22:57 +0800

    Fix boundary errors, but bad performance

--- hw2/hw2a.cc
+++ hw2/hw2a.cc
@@ -1,18 +1,15 @@
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
 #endif
-#include <boost/range/irange.hpp>
 #include <chrono>
 #include <complex>
 #include <iostream>
 #include <limits>
 #include <png.h>
 #include <pthread.h>
-#include <queue>
 #include <sched.h>
 #include <cassert>
 
-static void create_tasks(int image_width, int image_height);
 static void *producer(void *data);
 void process_mandelbrot_set();
 void write_png(const char *filename, int iters, int width, int height,
@@ -20,6 +17,8 @@ void write_png(const char *filename, int iters, int width, int height,
 
 struct schedule_data {
     int x0, y0, x1, y1;
+    schedule_data(int x0, int y0, int x1, int y1) : 
+    x0(x0), y0(y0), x1(x1), y1(y1) {}
 };
 
 double left, right;
@@ -28,8 +27,9 @@ int width, height;
 int iters;
 int num_thread;
 int *image(nullptr);
+int scheduled_idx(0);
 
-static std::queue<schedule_data> task_queue;
+const int tile_size = 200;
 pthread_mutex_t *mutex;
 
 int main(int argc, char **argv) {
@@ -61,52 +61,30 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-static void create_tasks(int image_width, int image_height) {
-    const int tile_size(200);
-    int task_count = (width * height) / tile_size;
-    int res = (width * height) % tile_size;
-
-    int sche_x(0), sche_y(0);
-    int scheduled_idx(0);
-    for(int i = 0; i < task_count; ++i) {
-        int xi = ((scheduled_idx) / image_height);
-        int yi = (scheduled_idx) % image_height;
-        scheduled_idx += tile_size;
-        assert(scheduled_idx <= (width * height));
-        int xf = ((scheduled_idx - 1) / image_height);
-        int yf = (scheduled_idx - 1) % image_height;
-        schedule_data task;
-        task.x0 = xi;
-        task.x1 = xf;
-        task.y0 = yi;
-        task.y1 = yf;
-        task_queue.push(task);
-    }
-    if (res) {
-        int xi = ((scheduled_idx) / image_height);
-        int yi = (scheduled_idx) % image_height;
-        int xf = width - 1;
-        int yf = height - 1;
-        schedule_data task;
-        task.x0 = xi;
-        task.x1 = xf;
-        task.y0 = yi;
-        task.y1 = yf;
-        task_queue.push(task);
-    }
-}
-
 static void *producer(void *data) {
     while (1) {
         pthread_mutex_lock(mutex);
-        if (task_queue.empty()) {
+        if (scheduled_idx >= (width * height)) {
             pthread_mutex_unlock(mutex);
             break;
         }
-        schedule_data job_data;
-        job_data = task_queue.front();
-        task_queue.pop();
+        int start = scheduled_idx;
+        scheduled_idx += tile_size;
+        std::cout << "Thread schedule_idx: " << start <<std::endl;
+        std::cout << "Updated idx: " << scheduled_idx << std::endl;
         pthread_mutex_unlock(mutex);
+
+        schedule_data job_data(0, 0, 0, 0);
+        job_data.x0 = ((start) / height);
+        job_data.y0 = (start) % height;
+        start += tile_size;
+        job_data.x1 = ((start-1) / height);
+        job_data.y1 = (start-1) % height;
+        if(start >= width * height) {
+            job_data.x1 = width - 1;
+            job_data.y1 = height - 1;
+        }
+        std::cout << "Schedule x0,y0,x1,y1: "<< job_data.x0 << ", "<< job_data.y0 << ", "<< job_data.x1 << ", "<< job_data.y1 << std::endl;
 
         if(job_data.x0 == job_data.x1) {
             for (int j = job_data.y0; j <= job_data.y1; ++j) {
@@ -190,19 +168,7 @@ static void *producer(void *data) {
 }
 
 void process_mandelbrot_set() {
-    create_tasks(width, height);
     pthread_t producer_threads[num_thread];
-
-    // Debug
-    // std::cout << "Print schedule" << std::endl;
-    // int size = task_queue.size();
-    // for(int i = 0; i < size; ++i) {
-    //     schedule_data backup = task_queue.front();
-    //     std::cout<< task_queue.front().x0<< ", " << task_queue.front().y0 << ", " << task_queue.front().x1<< ", " << task_queue.front().y1<< ", \n"; 
-    //     task_queue.pop();
-    //     task_queue.push(backup);
-    // }
-    //exit(0);
 
     for (int i = 0; i < num_thread; i++) {
         pthread_create(&producer_threads[i], NULL, producer, NULL);
@@ -229,8 +195,7 @@ void write_png(const char *filename, int iters, int width, int height,
     size_t row_size = 3 * width * sizeof(png_byte);
     png_bytep row = (png_bytep)malloc(row_size);
     for (int y = 0; y < height; ++y) {
-        memset(row, 0, row_size);
-        // fill(row, row+row_size, 0);
+        std::fill(row, row+row_size, 0);
         for (int x = 0; x < width; ++x) {
             int p = buffer[(height - 1 - y) * width + x];
             png_bytep color = row + x * 3;
