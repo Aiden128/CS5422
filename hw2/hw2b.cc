05f182f8f26f6cac2d02ceab1fdff9dc15bca7f1
jerryzj <jerry0715no14@gmail.com>
2019-11-07 23:01:52 +0800

    [Bug] manager doesn't stop!!!

--- hw2/hw2b.cc
+++ hw2/hw2b.cc
@@ -4,36 +4,38 @@
 #ifdef PERF
 //#include "perf.hpp"
 #endif
+#include "util.h"
+#include <algorithm>
+#include <boost/range/irange.hpp>
+#include <cassert>
 #include <chrono>
 #include <complex>
 #include <iostream>
+#include <iterator>
 #include <limits>
+#include <mpi.h>
+#include <omp.h>
 #include <png.h>
 #include <pthread.h>
 #include <sched.h>
-#include <cassert>
-#include <boost/range/irange.hpp>
-#include <mpi.h>
-#include <omp.h>
 
-int tile_size;
-int data_size;
-double left, right;
-double lower, upper;
-double dx, dy;
-int width, height;
-int image_size;
-int iters;
-int num_thread;
-int rank, task_num;
-int *image(nullptr);
-int tile_size(100);
+// Image information
+SetOnce<double> left, right;
+SetOnce<double> lower, upper;
+SetOnce<double> dx, dy;
+SetOnce<int> width, height;
+SetOnce<int> image_size, iters, num_thread;
+// Image partition information
+SetOnce<int> job_width;
+SetOnce<int> data_size;
+// MPI, OpenMP information
 const int MANAGER_ID(0);
-enum tag {RESULT, DATA, TERMINATE};
+int rank, task_num;
+enum tag { RESULT, DATA, TERMINATE };
 
-void manager();
+void manager(int *image);
 void worker();
-void mandel_omp(const int &start_idx = 0, const int &end_idx = image_size, int *result_ptr);
+void mandel_omp(const int &start_idx, int *result_ptr);
 void write_png(const char *filename, int iters, int width, int height,
                const int *buffer);
 
@@ -46,22 +48,22 @@ int main(int argc, char **argv) {
     upper = strtod(argv[6], 0);
     width = strtol(argv[7], 0, 10);
     height = strtol(argv[8], 0, 10);
+    dx = static_cast<double>((right - left) / width);
+    dy = static_cast<double>((upper - lower) / height);
     image_size = width * height;
-    image = new int[image_size];
-    dx = static_cast<double> ((right - left) / width);
-    dy = static_cast<double> ((upper - lower) / height);
 
     // MPI initialize
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &task_num);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // tile size
+    job_width = task_num == 1 ? height.get() : 1;
+    data_size = job_width * width + 1;
 
-    tile_size = task_num == 1 ? width : 20;
-    data_size = tile_size * height + 1;
-
-    if(rank == 0) {
+    if (rank == 0) {
+        int *image(new int[image_size.get()]);
         // Manager
-        manager();
+        manager(image);
         // Note that write png can be done in rank 0
         write_png(filename, iters, width, height, image);
         delete[](image);
@@ -74,51 +76,79 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void manager() {
+void manager(int *image) {
+    int *buffer(new int[data_size.get()]);
     // Single node
     if (task_num == 1) {
         // Draw image use normal OpenMP
-        mandel_omp(0, image_size, image);
+        mandel_omp(0, buffer);
+        // Store buffer data to png file
+        int col_idx = buffer[0];
+        int *color(buffer + 1);
+        int *image_dest = image + (col_idx * width);
+        std::copy_n(color, (data_size - 1), image_dest);
         return;
-    // Multi-node
+        // Multi-node
     } else {
         MPI_Status status;
         int active_nodes(1), schedule_idx(0);
-        while(active_nodes < task_num && schedule_idx < width) {
-            ++active_nodes;
-            schedule_idx += tile_size;
-            MPI_Send(&schedule_idx, 1, MPI::INT, active_nodes, tag::DATA, MPI_COMM_WORLD);
+        for (;active_nodes < task_num && schedule_idx < width;
+             schedule_idx += job_width, active_nodes++) {
+            MPI_Send(&schedule_idx, 1, MPI::INT, active_nodes, tag::DATA,
+                     MPI_COMM_WORLD);
         }
         do {
-            MPI_Recv(image, image_size, MPI::INT, MPI_ANY_SOURCE, tag::RESULT, MPI_COMM_WORLD, &status);
-            --active_nodes;
-            int sender_id (status.MPI_SOURCE);
-            int col_idx = image[0];
-            int *color(image + 1);
-            if (schedule_idx < width) {
-                MPI_Send(&schedule_idx, 1, MPI::INT, sender_id, tag::DATA, MPI_COMM_WORLD);
+            MPI_Recv(buffer, data_size, MPI::INT, MPI_ANY_SOURCE, tag::RESULT,
+                     MPI_COMM_WORLD, &status);
+            active_nodes--;
+            int sender_id(status.MPI_SOURCE);
+            int col_idx(buffer[0]);
+            int *color(buffer + 1);
+            int *image_dest(image + (col_idx * width));
+            if (schedule_idx < width.get()) {
+                MPI_Send(&schedule_idx, 1, MPI::INT, sender_id, tag::DATA,
+                         MPI_COMM_WORLD);
+                ++active_nodes;
+                schedule_idx += job_width;
             } else {
-                MPI_Send(&schedule_idx, 1, MPI::INT, sender_id, tag::TERMINATE, MPI_COMM_WORLD);
+                schedule_idx = -1;
+                MPI_Send(&schedule_idx, 1, MPI::INT, sender_id, tag::TERMINATE,
+                         MPI_COMM_WORLD);
             }
-        } while(active_nodes > 1);
+            std::copy_n(color, (data_size - 1), image_dest);
+        } while (active_nodes > 1);
     }
+    delete[](buffer);
 }
 
 void worker() {
+    int *buffer(new int[data_size.get()]);
     MPI_Status status;
-    int col_idx;
-    MPI_Recv(&col_idx, 1, MPI::INT, MANAGER_ID, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-    while(status.MPI_TAG == tag::DATA) {
-        mandel_omp(col_idx, col_idx + tile_size, image);
-        MPI_Send(image, data_size, MPI::INT, MANAGER_ID, tag::RESULT, MPI_COMM_WORLD);
-        MPI_Recv(&col_idx, 1, MPI::INT, MANAGER_ID, tag::DATA, MPI_COMM_WORLD, &status);
+    int col_idx(0);
+    MPI_Recv(&col_idx, 1, MPI::INT, MANAGER_ID, MPI_ANY_TAG, MPI_COMM_WORLD,
+             &status);
+
+    while (status.MPI_TAG == tag::DATA) {
+        mandel_omp(col_idx, buffer);
+        MPI_Send(buffer, data_size, MPI::INT, MANAGER_ID, tag::RESULT,
+                 MPI_COMM_WORLD);
+        MPI_Recv(&col_idx, 1, MPI::INT, MANAGER_ID, tag::DATA, MPI_COMM_WORLD,
+                 &status);
+        if(col_idx == -1) break;
     }
+    delete[](buffer);
 }
 
-void mandel_omp(const int &start_idx, const int &end_idx, int *result_ptr) {
-    #pragma omp parallel for schedule(dynamic, 100)
-    for(int pixel_idx = start_idx; pixel_idx < end_idx; ++pixel_idx) {
-        double y0((pixel_idx / width) * dy + lower), x0((pixel_idx % width) * dx + left);
+void mandel_omp(const int &start_idx, int *result_ptr) {
+    // result_ptr[0] holds col information
+    // actual pixel start from result_ptr[1]
+    int *color(result_ptr + 1);
+    result_ptr[0] = start_idx;
+
+#pragma omp parallel for schedule(dynamic, 100)
+    for (int pixel_idx = 0; pixel_idx < (data_size - 1); ++pixel_idx) {
+        double y0((pixel_idx / width) * dy + lower),
+            x0((pixel_idx % width) * dx + left);
         int repeats(0);
         double x(0.0), y(0.0), length_squared(0.0);
         while (repeats < iters && length_squared < 4) {
@@ -128,7 +158,7 @@ void mandel_omp(const int &start_idx, const int &end_idx, int *result_ptr) {
             length_squared = x * x + y * y;
             ++repeats;
         }
-        image[pixel_idx] = repeats;
+        color[pixel_idx] = repeats;
     }
 }
 
@@ -150,9 +180,8 @@ void write_png(const char *filename, int iters, int width, int height,
     png_set_compression_level(png_ptr, 1);
     size_t row_size = 3 * width * sizeof(png_byte);
     png_bytep row = (png_bytep)malloc(row_size);
-    #pragma unroll 2
     for (int y = 0; y < height; ++y) {
-        std::fill(row, row+row_size, 0);
+        std::fill(row, row + row_size, 0);
         for (int x = 0; x < width; ++x) {
             int p = buffer[(height - 1 - y) * width + x];
             png_bytep color = row + x * 3;
