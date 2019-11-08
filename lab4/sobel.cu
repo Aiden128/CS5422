bc7c562cf2bd619f961eb796fe95bfc838cb8dd8
jerryzj <jerry0715no14@gmail.com>
2019-11-08 18:30:54 +0800

    Finish lab4

--- lab4/sobel.cu
+++ lab4/sobel.cu
@@ -1,94 +1,113 @@
-#include <stdlib.h>
-#include <assert.h>
-#include <math.h>
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <cmath>
 #include "utils.h"
 
-#define Z 2
-#define Y 5
-#define X 5
-#define xBound X / 2
-#define yBound Y / 2
-#define SCALE 8
+const int X = 5;
+const int Y = 5;
+const int Z = 2;
+const int SCALE = 8;
 
-const char filter[Z][Y][X] = { { { -1, -4, -6, -4, -1 },
-                                 { -2, -8, -12, -8, -2 },
-                                 { 0, 0, 0, 0, 0 },
-                                 { 2, 8, 12, 8, 2 },
-                                 { 1, 4, 6, 4, 1 } },
-                               { { -1, -2, 0, 2, 1 },
-                                 { -4, -8, 0, 8, 4 },
-                                 { -6, -12, 0, 12, 6 },
-                                 { -4, -8, 0, 8, 4 },
-                                 { -1, -2, 0, 2, 1 } } };
+__device__ const int dev_filter[Z][X][Y] = { 
+    {{ -1, -4, -6, -4, -1},
+     { -2, -8,-12, -8, -2},
+     {  0,  0,  0,  0,  0}, 
+     {  2,  8, 12,  8,  2}, 
+     {  1,  4,  6,  4,  1}},
+    {{ -1, -2,  0,  2,  1}, 
+     { -4, -8,  0,  8,  4}, 
+     { -6,-12,  0, 12,  6}, 
+     { -4, -8,  0,  8,  4}, 
+     { -1, -2,  0,  2,  1}} 
+};
 
-inline int bound_check(int val, int lower, int upper) {
-    if (val >= lower && val < upper)
-        return 1;
-    else
-        return 0;
-}
+void __global__ sobel (unsigned char *s, unsigned char *t, unsigned height, unsigned width, unsigned channels, size_t pitch) {
+    int  x, y, i, v, u;
+    int  R, G, B;
+    double val[Z*3] = {0.0};
+    double totalR = 0.0;
+    double totalG = 0.0;
+    double totalB = 0.0;
+    int adjustX, adjustY, xBound, yBound;
 
-void sobel(unsigned char *s, unsigned char *t, unsigned height, unsigned width,
-           unsigned channels) {
-    double val[Z][3];
-    for (int y = 0; y < height; ++y) {
-        for (int x = 0; x < width; ++x) {
-            /* Z axis of filter */
-            for (int i = 0; i < Z; ++i) {
+    // Copy sobel mask to each thread
+    __shared__ int mask[Z][X][Y];
+    x = (blockIdx.x * blockDim.x + threadIdx.x) % Z;
+    y = (blockIdx.x * blockDim.x + threadIdx.x) / Z % X;
+    i = (blockIdx.x * blockDim.x + threadIdx.x) / Z / X % Y;
+    mask[x][y][i] = dev_filter[x][y][i];
+    __syncthreads();
 
-                val[i][2] = 0.;
-                val[i][1] = 0.;
-                val[i][0] = 0.;
+    y = blockIdx.x * blockDim.x + threadIdx.x + 2;
+    if (y >= height + 2) {
+        return;
+    }
+    for (x = 2; x < width + 2; ++x) {
+        for (i = 0; i < Z; ++i) {
+            // inline function causes compile err...
+            // Use adjust factor replace
+            adjustX = (X % 2) ? 1 : 0;
+            adjustY = (Y % 2) ? 1 : 0;
+            xBound = X / 2;
+            yBound = Y / 2;
 
-                /* Y and X axis of filter */
-                for (int v = -yBound; v <= yBound; ++v) {
-                    for (int u = -xBound; u <= xBound; ++u) {
-                        if (bound_check(x + u, 0, width) &&
-                            bound_check(y + v, 0, height)) {
-                            const unsigned char R =
-                                s[channels * (width * (y + v) + (x + u)) + 2];
-                            const unsigned char G =
-                                s[channels * (width * (y + v) + (x + u)) + 1];
-                            const unsigned char B =
-                                s[channels * (width * (y + v) + (x + u)) + 0];
-                            val[i][2] += R * filter[i][u + xBound][v + yBound];
-                            val[i][1] += G * filter[i][u + xBound][v + yBound];
-                            val[i][0] += B * filter[i][u + xBound][v + yBound];
-                        }
-                    }
+            val[i * 3 + 2] = 0.0;
+            val[i * 3 + 1] = 0.0;
+            val[i * 3] = 0.0;
+            for (v = -yBound; v < yBound + adjustY; ++v) {
+                for (u = -xBound; u < xBound + adjustX; ++u) {
+                        R = s[pitch * (y+v) + channels * (x + u) + 2];
+                        G = s[pitch * (y+v) + channels * (x + u) + 1];
+                        B = s[pitch * (y+v) + channels * (x + u) + 0];
+                        val[i * 3 + 2] += R * mask[i][u + xBound][v + yBound];
+                        val[i * 3 + 1] += G * mask[i][u + xBound][v + yBound];
+                        val[i * 3 + 0] += B * mask[i][u + xBound][v + yBound];
                 }
             }
-            double totalR = 0.;
-            double totalG = 0.;
-            double totalB = 0.;
-            for (int i = 0; i < Z; ++i) {
-                totalR += val[i][2] * val[i][2];
-                totalG += val[i][1] * val[i][1];
-                totalB += val[i][0] * val[i][0];
-            }
-            totalR = sqrt(totalR) / SCALE;
-            totalG = sqrt(totalG) / SCALE;
-            totalB = sqrt(totalB) / SCALE;
-            const unsigned char cR = (totalR > 255.) ? 255 : totalR;
-            const unsigned char cG = (totalG > 255.) ? 255 : totalG;
-            const unsigned char cB = (totalB > 255.) ? 255 : totalB;
-            t[channels * (width * y + x) + 2] = cR;
-            t[channels * (width * y + x) + 1] = cG;
-            t[channels * (width * y + x) + 0] = cB;
         }
+        totalR = 0.0;
+        totalG = 0.0;
+        totalB = 0.0;
+        for (i = 0; i < Z; ++i) {
+            totalR += val[i * 3 + 2] * val[i * 3 + 2];
+            totalG += val[i * 3 + 1] * val[i * 3 + 1];
+            totalB += val[i * 3 + 0] * val[i * 3 + 0];
+        }
+        totalR = sqrt(totalR) / SCALE;
+        totalG = sqrt(totalG) / SCALE;
+        totalB = sqrt(totalB) / SCALE;
+        t[pitch * y + channels * x + 2] = (totalR > 255.0) ? 255 : totalR;
+        t[pitch * y + channels * x + 1] = (totalG > 255.0) ? 255 : totalG;
+        t[pitch * y + channels * x + 0] = (totalB > 255.0) ? 255 : totalB;
     }
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char** argv) {
+
     assert(argc == 3);
     unsigned height, width, channels;
-    unsigned char *src = NULL, *dst;
-    /* read the image to src, and get height, width, channels */
-    read_png(argv[1], &src, &height, &width, &channels);
-    dst = (unsigned char *)malloc(height * width * channels *
-                                  sizeof(unsigned char));
-    /* computation */
-    sobel(src, dst, height, width, channels);
-    write_png(argv[2], dst, height, width, channels);
+    unsigned char* host_s(nullptr);
+    unsigned char *device_s(nullptr);
+    unsigned char *device_t(nullptr);
+    size_t pitch;
+    read_png(argv[1], &host_s, &height, &width, &channels);
+
+    // Reserve 2 pixels for each edge to prevent size becomes smaller after conv
+    cudaMallocPitch(&device_s, &pitch, (width+4) * sizeof(unsigned char)* channels, (height+4));
+    cudaMallocPitch(&device_t, &pitch,(width+4) * sizeof(unsigned char)* channels, (height+4));
+    unsigned char* host_t = (unsigned char*) malloc(height * width * channels * sizeof(unsigned char));
+
+    cudaMemcpy2D(device_s+2*pitch+2*channels, pitch, host_s, width * sizeof(unsigned char)* channels ,width * channels * sizeof(unsigned char), height, cudaMemcpyHostToDevice);
+    sobel<<<(height/128) + 1, 128>>>(device_s, device_t, height, width, channels,pitch);
+    cudaMemcpy2D(host_t , width * sizeof(unsigned char)* channels, device_t+2*pitch+2*channels, pitch , width * channels * sizeof(unsigned char), height, cudaMemcpyDeviceToHost);
+
+    write_png(argv[2], host_t, height, width, channels);
+
+    cudaFree(device_s);
+    cudaFree(device_t);
+    cudaFreeHost(host_s);
+    free(host_t);
+
     return 0;
 }
