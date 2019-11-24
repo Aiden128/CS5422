1d996e78f708052cfabc6af7e37f5d5dc0614e8a
jerryzj <jerry0715no14@gmail.com>
2019-11-24 16:34:55 +0800

    hw3 RC1

--- hw3/hw3.cc
+++ hw3/hw3.cc
@@ -1,118 +1,93 @@
-//#include "floyd_warshall.h"
+#include "tbb/task_scheduler_init.h"
 #include "tbb/tbb.h"
 #include <cstdint>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
 #include <limits>
-#include <vector>
 #include <omp.h>
 
-const int32_t MaxDistance = INT16_MAX;
-const int32_t Out_max = 1073741823;
+const int32_t INF = 1073741823;
 
-class APSP {
-  private:
-    int32_t num_vertex;
-    std::vector<std::vector<int32_t>> AdjMatrix;
-
-  public:
-    APSP() : num_vertex(0){};
-    APSP(int32_t n);
-    inline void AddEdge(int32_t from, int32_t to, int32_t weight);
-    void PrintData(std::vector<std::vector<int32_t>> array);
-    void Write_file(const char *filename);
-    void FloydWarshall();
-    void Parallel_FloydWarshall();
-};
-
-APSP::APSP(int32_t n) : num_vertex(n) {
-    // Constructor, initialize AdjMatrix with 0 or MaxDistance
-    AdjMatrix.resize(num_vertex);
-#pragma omp parallel for 
-    for (int32_t i = 0; i < num_vertex; i++) {
-        AdjMatrix[i].resize(num_vertex, MaxDistance);
-        for (int32_t j = 0; j < num_vertex; j++) {
-            if (i == j) {
-                AdjMatrix[i][j] = 0;
-            }
-        }
-    }
-}
-
-void APSP::FloydWarshall() {
-    for (int32_t k = 0; k < num_vertex; k++) {
-        for (int32_t i = 0; i < num_vertex; i++) {
-            for (int32_t j = 0; j < num_vertex; j++) {
-                AdjMatrix[i][j] = std::min(AdjMatrix[i][j], (AdjMatrix[i][k] + AdjMatrix[k][j]));
-            }
-        }
-    }
-}
-
-void APSP::Parallel_FloydWarshall() {
-    int32_t i, j, k;
-
-#pragma omp parallel shared(num_vertex, AdjMatrix) private(i, j, k) default(none)
-    for (k = 0; k < num_vertex; k++) {
-#pragma omp for schedule(dynamic)
-        for (i = 0; i < num_vertex; i++) {
-            for (j = 0; j < num_vertex; j++) {
-                AdjMatrix[i][j] = std::min(AdjMatrix[i][j], (AdjMatrix[i][k] + AdjMatrix[k][j]));
-            }
-        }
-    }
-}
-
-void APSP::PrintData(std::vector<std::vector<int32_t>> array) {
-    for (int32_t i = 0; i < num_vertex; i++) {
-        for (int32_t j = 0; j < num_vertex; j++) {
-            std::cout << std::setw(5) << array[i][j];
-        }
-        std::cout << std::endl;
-    }
-}
-
-inline void APSP::AddEdge(int32_t from, int32_t to, int32_t weight) {
-    AdjMatrix[from][to] = weight;
-}
-
-void APSP::Write_file(const char *filename) {
+void Write_file(const char *filename, int *AdjMatrix, const int &num_vertex) {
     std::ofstream out_file(filename);
     for (int32_t i = 0; i < num_vertex; i++) {
-        for (int32_t j = 0; j < num_vertex; j++) {
-            if (AdjMatrix[i][j] == INT16_MAX) {
-                // Write 2^30 -1
-                out_file.write((char *)&Out_max, sizeof(Out_max));
-            } else {
-                // Write normal
-                out_file.write((char *)&AdjMatrix[i][j], sizeof(int32_t));
-            }
-        }
+        out_file.write((char *)&AdjMatrix[i * num_vertex],
+                       sizeof(int) * num_vertex);
     }
     out_file.close();
 }
 
-int main(int argc, char **argv) {
+void floyd_warshall_parasimd(int *distance, size_t n) {
+    int thread_count = omp_get_max_threads();
+    tbb::task_scheduler_init init(thread_count);
+    int grain_size = n / thread_count;
+    for (size_t k = 0; k < n; ++k) {
+        tbb::parallel_for(
+            tbb::blocked_range<int>{0, static_cast<int>(n),
+                                    static_cast<size_t>(grain_size)},
+            [&](auto r) {
+                for (int i = r.begin(); i < r.end(); ++i) {
+                    // ispc::floyd_warshall_simd(distance, n, k, i);
+                    for (int j = 0; j < n; ++j) {
+                        distance[i * n + j] = std::min(
+                            distance[i * n + j],
+                            (distance[i * n + k] + distance[k * n + j]));
+                    }
+                }
+            });
+    }
+}
 
+void floyd_warshall_omp(int *distance, size_t n) {
+    int thread_count = omp_get_max_threads();
+    int grain(n / thread_count);
+    for (int k = 0; k < n; k++) {
+#pragma omp parallel num_threads(thread_count)
+        {
+            int tid = omp_get_thread_num();
+            int start = grain * tid;
+            int end = tid == thread_count - 1 ? n : grain * (tid + 1);
+            for (int i = start; i < end; ++i) {
+                // ispc::floyd_warshall_simd(distance, n, k, i);
+                for (int j = 0; j < n; ++j) {
+                    distance[i * n + j] =
+                        std::min(distance[i * n + j],
+                                 (distance[i * n + k] + distance[k * n + j]));
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char **argv) {
     std::ifstream file(argv[1]);
     int32_t num_vertex(0), num_edge(0);
     int32_t src(0), dest(0), weight(0);
 
-    file.seekg(0, std::ios_base::beg);
     file.read((char *)&num_vertex, sizeof(num_vertex));
-    APSP apsp(num_vertex);
     file.read((char *)&num_edge, sizeof(num_edge));
+    int *AdjMatrix = new int[num_vertex * num_vertex]();
+
+    std::fill_n(AdjMatrix, num_vertex * num_vertex, INF);
+    for (int32_t i = 0; i < num_vertex; i++) {
+        AdjMatrix[i * num_vertex + i] = 0;
+    }
+
+    int *tmp(new int[num_edge * 3]);
+    file.read((char *)tmp, sizeof(int) * num_edge * 3);
     for (int32_t i = 0; i < num_edge; ++i) {
-        file.read((char *)&src, sizeof(src));
-        file.read((char *)&dest, sizeof(dest));
-        file.read((char *)&weight, sizeof(weight));
-        apsp.AddEdge(src, dest, weight);
+        src = tmp[i * 3];
+        dest = tmp[i * 3 + 1];
+        weight = tmp[i * 3 + 2];
+        int idx(src * num_vertex + dest);
+        AdjMatrix[idx] = weight;
     }
     file.close();
+    floyd_warshall_omp(AdjMatrix, num_vertex);
+    Write_file(argv[2], AdjMatrix, num_vertex);
 
-    //apsp.FloydWarshall();
-    apsp.Parallel_FloydWarshall();
-    apsp.Write_file(argv[2]);
+    delete[](AdjMatrix);
+    delete[](tmp);
     return 0;
 }
