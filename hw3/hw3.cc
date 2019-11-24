ca9eeae6d407308b9c6f788897c177837d5cd7b3
jerryzj <jerry0715no14@gmail.com>
2019-11-24 16:53:33 +0800

    hw3 RC2

--- hw3/hw3.cc
+++ hw3/hw3.cc
@@ -7,11 +7,11 @@
 #include <limits>
 #include <omp.h>
 
-const int32_t INF = 1073741823;
+const int INF = 1073741823;
 
 void Write_file(const char *filename, int *AdjMatrix, const int &num_vertex) {
     std::ofstream out_file(filename);
-    for (int32_t i = 0; i < num_vertex; i++) {
+    for (int i = 0; i < num_vertex; i++) {
         out_file.write((char *)&AdjMatrix[i * num_vertex],
                        sizeof(int) * num_vertex);
     }
@@ -28,7 +28,6 @@ void floyd_warshall_parasimd(int *distance, size_t n) {
                                     static_cast<size_t>(grain_size)},
             [&](auto r) {
                 for (int i = r.begin(); i < r.end(); ++i) {
-                    // ispc::floyd_warshall_simd(distance, n, k, i);
                     for (int j = 0; j < n; ++j) {
                         distance[i * n + j] = std::min(
                             distance[i * n + j],
@@ -49,7 +48,6 @@ void floyd_warshall_omp(int *distance, size_t n) {
             int start = grain * tid;
             int end = tid == thread_count - 1 ? n : grain * (tid + 1);
             for (int i = start; i < end; ++i) {
-                // ispc::floyd_warshall_simd(distance, n, k, i);
                 for (int j = 0; j < n; ++j) {
                     distance[i * n + j] =
                         std::min(distance[i * n + j],
@@ -62,21 +60,21 @@ void floyd_warshall_omp(int *distance, size_t n) {
 
 int main(int argc, char **argv) {
     std::ifstream file(argv[1]);
-    int32_t num_vertex(0), num_edge(0);
-    int32_t src(0), dest(0), weight(0);
+    int num_vertex(0), num_edge(0);
+    int src(0), dest(0), weight(0);
 
     file.read((char *)&num_vertex, sizeof(num_vertex));
     file.read((char *)&num_edge, sizeof(num_edge));
     int *AdjMatrix = new int[num_vertex * num_vertex]();
 
     std::fill_n(AdjMatrix, num_vertex * num_vertex, INF);
-    for (int32_t i = 0; i < num_vertex; i++) {
+    for (int i = 0; i < num_vertex; i++) {
         AdjMatrix[i * num_vertex + i] = 0;
     }
 
     int *tmp(new int[num_edge * 3]);
     file.read((char *)tmp, sizeof(int) * num_edge * 3);
-    for (int32_t i = 0; i < num_edge; ++i) {
+    for (int i = 0; i < num_edge; ++i) {
         src = tmp[i * 3];
         dest = tmp[i * 3 + 1];
         weight = tmp[i * 3 + 2];
